34de88bb06eade2fb49ed8edcca365950e82a02c
Cecile Vang <Keycie Chang>
2024-11-24 23:01:33 +0100

    Premier commit : ajout des fichiers du site

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script pour un futur ajout de fonctionnalités comme des animations ou interactions
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Bienvenue sur mon portfolio interactif !");
+});
